4e4c5dd7a400d284bbd40385a4b060934b6805d5
Muzaffer Aydin <Muzaffer.Aydin@improving.com>
2024-08-29 16:10:57 -0700

    [MigrationApp.GUI] - UI elements are arranged according to Endpoint option entites

--- src/MigrationApp.GUI/ViewModels/MainWindowViewModel.cs
+++ src/MigrationApp.GUI/ViewModels/MainWindowViewModel.cs
@@ -9,73 +9,105 @@ namespace MigrationApp.GUI.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase, INotifyDataErrorInfo
     {
-        private string _serverUrl = string.Empty;
-        private string _adminUserName = string.Empty;
-        private string _personalAccessToken = string.Empty;
-        private string _cloudUrl = string.Empty;
-        private string _adminUserEmail = string.Empty;
-        private string _cloudPersonalAccessToken = string.Empty;
+        private string _serverUri = string.Empty;
+        private string _serverSiteContent = string.Empty;
+        private string _serverAccessTokenName = string.Empty;
+        private string _serverAccessToken = string.Empty;
+        private string _cloudUri = string.Empty;
+        private string _cloudSiteContent = string.Empty;
+        private string _cloudAccessTokenName = string.Empty;
+        private string _cloudAccessToken = string.Empty;
+        private string _serverToCloudUserDomainMap = string.Empty;
 
         private readonly Dictionary<string, List<string>> _errors = new();
 
-        public string ServerUrl
+        public string ServerUri
         {
-            get => _serverUrl;
+            get => _serverUri;
             set
             {
-                SetProperty(ref _serverUrl, value);
-                ValidateRequiredField(value, nameof(ServerUrl), "Server URL is required.");
+                SetProperty(ref _serverUri, value);
+                ValidateRequiredField(value, nameof(ServerUri), "Tableau Server URI is required.");
             }
         }
 
-        public string AdminUserName
+        public string ServerSiteContent
         {
-            get => _adminUserName;
+            get => _serverSiteContent;
             set
             {
-                SetProperty(ref _adminUserName, value);
-                ValidateRequiredField(value, nameof(AdminUserName), "Admin Username is required.");
+                SetProperty(ref _serverSiteContent, value);
+                ValidateRequiredField(value, nameof(ServerSiteContent), "Tableau Server Site Content is required.");
             }
         }
 
-        public string PersonalAccessToken
+        public string ServerAccessTokenName
         {
-            get => _personalAccessToken;
+            get => _serverAccessTokenName;
             set
             {
-                SetProperty(ref _personalAccessToken, value);
-                ValidateRequiredField(value, nameof(PersonalAccessToken), "Personal Access Token is required.");
+                SetProperty(ref _serverAccessTokenName, value);
+                ValidateRequiredField(value, nameof(ServerAccessTokenName), "Tableau Server Access Token Name is required.");
             }
         }
 
-        public string CloudUrl
+        public string ServerAccessToken
         {
-            get => _cloudUrl;
+            get => _serverAccessToken;
             set
             {
-                SetProperty(ref _cloudUrl, value);
-                ValidateRequiredField(value, nameof(CloudUrl), "Cloud URL is required.");
+                SetProperty(ref _serverAccessToken, value);
+                ValidateRequiredField(value, nameof(ServerAccessToken), "Tableau Server Access Token is required.");
             }
         }
 
-        public string AdminUserEmail
+        public string CloudUri
         {
-            get => _adminUserEmail;
+            get => _cloudUri;
             set
             {
-                SetProperty(ref _adminUserEmail, value);
-                ValidateRequiredField(value, nameof(AdminUserEmail), "Admin User Email is required.");
-                ValidateEmail(value, nameof(AdminUserEmail), "Invalid email address.");
+                SetProperty(ref _cloudUri, value);
+                ValidateRequiredField(value, nameof(CloudUri), "Tableau Cloud URI is required.");
             }
         }
 
-        public string CloudPersonalAccessToken
+        public string CloudSiteContent
         {
-            get => _cloudPersonalAccessToken;
+            get => _cloudSiteContent;
             set
             {
-                SetProperty(ref _cloudPersonalAccessToken, value);
-                ValidateRequiredField(value, nameof(CloudPersonalAccessToken), "Cloud Personal Access Token is required.");
+                SetProperty(ref _cloudSiteContent, value);
+                ValidateRequiredField(value, nameof(CloudSiteContent), "Tableau Cloud Site Content is required.");
+            }
+        }
+
+        public string CloudAccessTokenName
+        {
+            get => _cloudAccessTokenName;
+            set
+            {
+                SetProperty(ref _cloudAccessTokenName, value);
+                ValidateRequiredField(value, nameof(CloudAccessTokenName), "Tableau Cloud Access Token Name is required.");
+            }
+        }
+
+        public string CloudAccessToken
+        {
+            get => _cloudAccessToken;
+            set
+            {
+                SetProperty(ref _cloudAccessToken, value);
+                ValidateRequiredField(value, nameof(CloudAccessToken), "Tableau Cloud Access Token is required.");
+            }
+        }
+
+        public string ServerToCloudUserDomainMap
+        {
+            get => _serverToCloudUserDomainMap;
+            set
+            {
+                SetProperty(ref _serverToCloudUserDomainMap, value);
+                ValidateRequiredField(value, nameof(ServerToCloudUserDomainMap), "Tableau Server to Cloud User Domain Map is required.");
             }
         }
 
@@ -88,18 +120,21 @@ namespace MigrationApp.GUI.ViewModels
 
         private void RunMigration()
         {
-            ValidateRequiredField(ServerUrl, nameof(ServerUrl), "Server URL is required.");
-            ValidateRequiredField(AdminUserName, nameof(AdminUserName), "Admin Username is required.");
-            ValidateRequiredField(PersonalAccessToken, nameof(PersonalAccessToken), "Personal Access Token is required.");
-            ValidateRequiredField(CloudUrl, nameof(CloudUrl), "Cloud URL is required.");
-            ValidateRequiredField(AdminUserEmail, nameof(AdminUserEmail), "Admin User Email is required.");
-            ValidateEmail(AdminUserEmail, nameof(AdminUserEmail), "Invalid email address.");
-            ValidateRequiredField(CloudPersonalAccessToken, nameof(CloudPersonalAccessToken), "Cloud Personal Access Token is required.");
+            ValidateRequiredField(ServerUri, nameof(ServerUri), "Tableau Server URI is required.");
+            ValidateRequiredField(ServerSiteContent, nameof(ServerSiteContent), "Tableau Server Site Content is required.");
+            ValidateRequiredField(ServerAccessTokenName, nameof(ServerAccessTokenName), "Tableau Server Access Token Name is required.");
+            ValidateRequiredField(ServerAccessToken, nameof(ServerAccessToken), "Tableau Server Access Token is required.");
+            ValidateRequiredField(CloudUri, nameof(CloudUri), "Tableau Cloud URI is required.");
+            ValidateRequiredField(CloudSiteContent, nameof(CloudSiteContent), "Tableau Cloud Site Content is required.");
+            ValidateRequiredField(CloudAccessTokenName, nameof(CloudAccessTokenName), "Tableau Cloud Access Token Name is required.");
+            ValidateRequiredField(CloudAccessToken, nameof(CloudAccessToken), "Tableau Cloud Access Token is required.");
+            ValidateRequiredField(ServerToCloudUserDomainMap, nameof(ServerToCloudUserDomainMap), "Tableau Server to Cloud User Domain Map is required.");
 
             if (HasErrors)
             {
                 return;
             }
+
             // Proceed with the migration process
         }
 
@@ -108,18 +143,6 @@ namespace MigrationApp.GUI.ViewModels
         private void ValidateRequiredField(string value, string propertyName, string errorMessage)
         {
             if (string.IsNullOrWhiteSpace(value))
-            {
-                AddError(propertyName, errorMessage);
-            }
-            else
-            {
-                RemoveError(propertyName, errorMessage);
-            }
-        }
-
-        private void ValidateEmail(string value, string propertyName, string errorMessage)
-        {
-            if (!string.IsNullOrWhiteSpace(value) && !value.Contains("@"))
             {
                 AddError(propertyName, errorMessage);
             }
