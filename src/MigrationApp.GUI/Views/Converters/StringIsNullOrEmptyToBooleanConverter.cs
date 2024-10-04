3f265614a2fd0326d04bb3cdc362df24f91a3ea6
Muzaffer Aydin <muzaffer.aydin88@gmail.com>
2024-10-03 17:11:20 -0700

    [TMAPP-58] Add ability to remove the link from help button text (#34)
    * [GUI] - Add StringIsNullOrEmptyToVisibilityConverter for conditional visibility of help button link and remove the link form User domain mapping help text
    
    ---------
    
    Co-authored-by: Muzaffer Aydin <Muzaffer.Aydin@improving.com>

---
+++ src/MigrationApp.GUI/Views/Converters/StringIsNullOrEmptyToBooleanConverter.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="StringIsNullOrEmptyToBooleanConverter.cs" company="Salesforce, inc.">
+// Copyright (c) Salesforce, inc.. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MigrationApp.GUI.Views.Converters
+{
+    using System;
+    using System.Globalization;
+    using Avalonia.Data.Converters;
+
+    /// <summary>
+    /// Converts a string value to a boolean based on whether it is null or empty.
+    /// Returns true if a non empty string value is set.
+    /// </summary>
+    public class StringIsNullOrEmptyToBooleanConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts the given string or null value to a boolean.
+        /// Returns true if a non empty string value is set.
+        /// </summary>
+        /// <param name="value">The object to be converted, expected to be a string or null.</param>
+        /// <param name="targetType">The target conversion type (not used in this implementation).</param>
+        /// <param name="parameter">An optional parameter (not used in this implementation).</param>
+        /// <param name="culture">The culture information for the conversion (not used in this implementation).</param>
+        /// <returns>
+        /// True if the string is not null or empty, otherwise false.
+        /// </returns>
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var input = value as string;
+            return !string.IsNullOrEmpty(input);
+        }
+
+        /// <summary>
+        /// Not needed for this converter.
+        /// </summary>
+        /// <param name="value">The object to convert back (not used).</param>
+        /// <param name="targetType">The type to convert back from (not used).</param>
+        /// <param name="parameter">The conversion parameter (not used).</param>
+        /// <param name="culture">The culture information for the conversion (not used).</param>
+        /// <returns>Throws <see cref="NotImplementedException"/> as this method is not supported.</returns>
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
